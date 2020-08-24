59e8c83708ee8a43f7530d8c376014d3d16cb04a
Ünver Can Ünlü <unverunlu@cfm.com.tr>
2020-08-24 17:19:20 +0300

    datetime format is updated to use hour value between 0-24.

--- src/main/java/tr/unvercanunlu/example/springbootsse/constant/DateTimeFormat.java
+++ src/main/java/tr/unvercanunlu/example/springbootsse/constant/DateTimeFormat.java
@@ -2,5 +2,5 @@ package tr.unvercanunlu.example.springbootsse.constant;
 
 public class DateTimeFormat {
 
-    public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd'T'hh:mm:ss.SSS";
+    public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS";
 }
