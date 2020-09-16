97945886f0c70cf37c620ba9d5ac6b585162d66a
Ünver Can Ünlü <unverunlu@cfm.com.tr>
2020-09-16 11:09:54 +0300

    event mapper is updated.

--- src/main/java/tr/unvercanunlu/example/springbootsse/mapper/EventMapper.java
+++ src/main/java/tr/unvercanunlu/example/springbootsse/mapper/EventMapper.java
@@ -5,27 +5,25 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter.SseEventBuilder;
-import tr.unvercanunlu.example.springbootsse.constant.DateTimeFormat;
-import tr.unvercanunlu.example.springbootsse.structure.Event;
+import tr.unvercanunlu.example.springbootsse.config.DateTimeFormat;
+import tr.unvercanunlu.example.springbootsse.model.Event;
 
 import java.time.format.DateTimeFormatter;
 
 @Component
-public class EventToSseEventMapper {
+public class EventMapper {
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(EventToSseEventMapper.class);
-
-    private static final DateTimeFormatter TIMESTAMP_FORMATTER = DateTimeFormatter.ofPattern(DateTimeFormat.TIMESTAMP_FORMAT);
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventMapper.class);
 
     public SseEventBuilder map(Event event, Long reconnectInMilliseconds) {
         SseEventBuilder sseEventBuilder = SseEmitter.event()
                 .name(event.getName().toLowerCase())
                 .data(event)
-                .id(event.getTimestamp().format(TIMESTAMP_FORMATTER))
+                .id(event.getTimestamp().format(DateTimeFormatter.ofPattern(DateTimeFormat.UTC_DATETIME_FORMAT)))
                 .comment(event.getDescription())
                 .reconnectTime(reconnectInMilliseconds);
 
-        LOGGER.info("Event is mapped to SseEvent: " + event.getName().toLowerCase());
+        LOGGER.debug("Event is mapped to SseEvent: " + event.getName().toLowerCase());
 
         return sseEventBuilder;
     }
